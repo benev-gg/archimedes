a74d5ee19328d04039a9e29453187070019ad674
Chase Moskal <chasemoskal@gmail.com>
2025-05-16 14:40:52 -0700

    fix: nanny's removeDuplicates

--- s/core/parts/parcels/utils/nanny.ts
+++ s/core/parts/parcels/utils/nanny.ts
@@ -4,8 +4,13 @@ import {Parcel} from "../types.js"
 export class Nanny {
 	biggest: number = -1
 
-	removeDuplicates = ([id]: Parcel<any>, _index: number, parcels: Parcel<any>[]) => {
-		return !parcels.some(p => p[0] === id)
+	removeDuplicates = ([idA]: Parcel<any>, _index: number, parcels: Parcel<any>[]) => {
+		let count = 0
+		for (const [idB] of parcels) {
+			if (idB === idA) count += 1
+			if (count > 1) return false
+		}
+		return true
 	}
 
 	removeDisorderly = ([id]: Parcel<any>) => {
