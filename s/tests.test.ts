9b149c279aa8908f27190418ba2b5f8c2a710eb7
Chase Moskal <chasemoskal@gmail.com>
2026-03-18 18:32:44 -0700

    reorganize: alpha

--- s/tests.test.ts
+++ s/tests.test.ts
@@ -4,7 +4,8 @@ import eureka from "./eureka/testing/eureka.test.js"
 import parcels from "./core/parcels/parcels.test.js"
 import contact from "./core/contact/contact.test.js"
 import integration from "./eureka/testing/integration.test.js"
-import nova from "./alpha/test.js"
+
+import nova from "./alpha/ecs/test.js"
 
 await Science.run({
 	nova,
