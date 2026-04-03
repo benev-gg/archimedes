8de09cf10128bb3ce0a885a0d6d05447f98d8a1d
Chase Moskal <chasemoskal@gmail.com>
2026-04-03 16:48:55 -0700

    delete: old ecs

--- s/ecs/index.ts
+++
@@ -1,7 +0,0 @@
-
-export * from "./parts/changers.js"
-export * as change from "./parts/changers.js"
-export * from "./parts/make-id.js"
-export * from "./parts/world.js"
-export * from "./parts/types.js"
-
--- s/ecs/utils/is-match.ts
+++
@@ -1,7 +0,0 @@
-
-import {Components} from "../parts/types.js"
-
-export function isMatch(set: Set<any>, components: Components) {
-	return [...set].every(key => key in components)
-}
-
