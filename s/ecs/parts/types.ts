d331f657ad7deca04e1e2f52ca28851cf56014d6
Chase Moskal <chasemoskal@gmail.com>
2026-04-04 22:13:49 -0700

    wip: commit-based

--- s/ecs/parts/types.ts
+++ s/ecs/parts/types.ts
@@ -3,10 +3,6 @@ export type Id = string
 export type Components = Record<string, unknown>
 export type AsComponents<C extends Components> = C
 export type Select<C extends Components, K extends keyof C> = Pick<C, K> & Partial<C>
-export type System<C extends Components> = () => Generator<Change<C>>
-
-export const asSystem = <C extends Components>(system: System<C>) => system
-export const asSystems = <C extends Components>(...systems: System<C>[]) => systems
 
 export enum ChangeKind {Set, Merge, Drop}
 export type ChangeSet<C extends Components> = [kind: ChangeKind.Set, id: Id, components?: Partial<C>]
@@ -14,12 +10,18 @@ export type ChangeMerge<C extends Components> = [kind: ChangeKind.Merge, id: Id,
 export type ChangeDrop<C extends Components> = [kind: ChangeKind.Drop, id: Id, keys: (keyof C)[]]
 export type Change<C extends Components> = ChangeSet<C> | ChangeMerge<C> | ChangeDrop<C>
 
+export type Commit<C extends Components> = (...changes: Change<C>[]) => void
+
+export type System<C extends Components> = (commit: Commit<C>) => void
+export const asSystem = <C extends Components>(system: System<C>) => system
+export const asSystems = <C extends Components>(...systems: System<C>[]) => systems
+
 export type LifecycleCallbacks<C extends Components, K extends keyof C> = {
 	tick: (id: Id, components: Select<C, K>) => void
 	exit: (id: Id) => void
 }
 
 export type LifecycleEnter<C extends Components, K extends keyof C> = (
-	(id: Id, components: Select<C, K>) => LifecycleCallbacks<C, K>
+	(id: Id, components: Select<C, K>, commit: Commit<C>) => LifecycleCallbacks<C, K>
 )
 
